4f19c99d78bc3db06cc5bef5aa6921169c71ae94
Flora-Pvt <f.preuvot@live.fr>
2020-11-13 12:35:01 -0500

    signup and authroute

--- src/redux/network/userNetwork.js
+++ src/redux/network/userNetwork.js
@@ -1,14 +1,18 @@
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from "../types";
+import {
+  SET_USER,
+  SET_ERRORS,
+  CLEAR_ERRORS,
+  LOADING_UI,
+  SET_UNAUTHENTICATED,
+} from "../types";
 import axios from "axios";
 
 export const loginUser = (userData, history) => (dispatch) => {
   dispatch({ type: LOADING_UI });
   axios
     .post("http://localhost:3000/api/auth/login", userData)
-    .then((res) => {
-      const token = `Bearer ${res.data.token}`;
-      localStorage.setItem("auth", JSON.stringify(res.data));
-      axios.defaults.headers.common["Authorization"] = token;
+    .then((auth) => {
+      setAuthorizationHeader(auth);
       dispatch(getUserData());
       dispatch({ type: CLEAR_ERRORS });
       history.push("/");
@@ -16,14 +20,32 @@ export const loginUser = (userData, history) => (dispatch) => {
     .catch((err) => {
       dispatch({
         type: SET_ERRORS,
-        payload: err.response.data,
+        payload: err.data,
+      });
+    });
+};
+
+export const signupUser = (newUserData, history) => (dispatch) => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .post("http://localhost:3000/api/auth/signup", newUserData)
+    .then((auth) => {
+      setAuthorizationHeader(auth);
+      dispatch(getUserData());
+      dispatch({ type: CLEAR_ERRORS });
+      history.push("/");
+    })
+    .catch((err) => {
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.data,
       });
     });
 };
 
 export const getUserData = () => (dispatch) => {
-  const auth = JSON.parse(localStorage.getItem('auth'))
-  const userId = auth.userId
+  const auth = JSON.parse(localStorage.getItem("auth"));
+  const userId = auth.userId;
   axios
     .get("http://localhost:3000/api/auth/" + userId)
     .then((res) => {
@@ -33,4 +55,16 @@ export const getUserData = () => (dispatch) => {
       });
     })
     .catch((err) => console.log(err));
+};
+
+export const logoutUser = () => (dispatch) => {
+  localStorage.removeItem("auth");
+  delete axios.defaults.headers.common["Authorization"];
+  dispatch({ type: SET_UNAUTHENTICATED });
+};
+
+const setAuthorizationHeader = (auth) => {
+  const token = `Bearer ${auth.data.token}`;
+  localStorage.setItem("auth", JSON.stringify(auth.data));
+  axios.defaults.headers.common["Authorization"] = token;
 };
